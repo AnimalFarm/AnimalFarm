8f39723a8d72910513223be2698cf94fbe97796a
NSH <gus9583@naver.com>
2015-09-09 05:50:47 +0900

    Git 설정,유니티 모바일 환경설정 중

---
+++ Assets/Script/PhonManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhonManager : MonoBehaviour{
+
+	void Start () 
+    {
+        Screen.orientation = ScreenOrientation.Landscape; //노승현, 게임 실행시 가로로 시작하는 설정
+        Screen.sleepTimeout = SleepTimeout.NeverSleep; //노승현, 핸드폰이 계속 켜져있게끔 하는 설정
+	}
+	void Update () 
+    {
+	
+	}
+}
