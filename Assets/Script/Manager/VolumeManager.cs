3749521ab3e0f76529c1080939b8192abb3d8ff1
OHJinSu <qaqa12300@naver.com>
2015-11-22 01:40:13 +0900

    UI 수정중

---
+++ Assets/Script/Manager/VolumeManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// 노승현 사운드 컨트롤 스크립트
+
+public class VolumeManager : MonoBehaviour {
+
+    public GameObject soundBar;
+    public bool soundCheck = true;
+    public AudioSource volumeControl;
+    public UISlider bgSoundSize;
+    public UILabel bgSizeFont;
+
+	
+	void Awake () 
+    {
+        volumeControl = GetComponent<AudioSource>();
+        bgSoundSize = soundBar.GetComponent<UISlider>();
+        volumeControl.volume = 1.0f;
+	}
+    
+    public void BGsoundSize() // 사운드 소리 크기
+    {
+        int size = (int)(bgSoundSize.value * 100);
+        bgSizeFont.text = size.ToString();
+
+        if (!soundCheck) return;
+        volumeControl.volume = bgSoundSize.value;
+    }
+    public void SoundOnOff()
+    {
+        soundCheck = !soundCheck;
+        if (soundCheck == true)
+        {
+            volumeControl.volume = bgSoundSize.value;
+        }
+        else
+            volumeControl.volume = 0;
+    }
+
+}
--- Assets/Script/RobbyButton.cs
+++ Assets/Script/RobbyButton.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class RobbyButton : MonoBehaviour {
 
-    public GameObject roomMake, roomSearch, shop, guild, profile, character;
-    public GameObject coinPlus, jewelPlus, mail, set;
-    public GameObject room;
-    public GameObject creatRoom, roomBorder, shopMenubor, characterShopUIPanel, setOption, Maliborder;
+    public GameObject roomSearch, shop, profile, character;
+    public GameObject coinPlus, jewelPlus, seting;
+    public GameObject shopMenubor, characterShopUIPanel, setOption, Maliborder;
     public GameObject modeling;
 
     public bool modelChack;
@@ -16,17 +15,7 @@ public class RobbyButton : MonoBehaviour {
         modelChack = false;
 	}
 	
-	// Update is called once per frame
-    public void RoomMake() // 오진수 방만들기
-    {
-        creatRoom.SetActive(true);
-        StopButton();
-    }
-    public void RoomBorder() // 오진수 방찾기
-    {
-        roomBorder.SetActive(true);
-        StopGameObjectButton();
-    }
+	// Update is called once per fram
     public void Shop() // 오진수 상점 열기
     {
         shopMenubor.SetActive(true);
@@ -43,12 +32,6 @@ public class RobbyButton : MonoBehaviour {
         setOption.SetActive(true);
         StopButton();
     }
-    public void OpenRoom() // 오진수 로비방 열기
-    {
-        creatRoom.SetActive(false);
-        room.SetActive(true);
-        StopGameObjectButton();
-    }
     public void ModelOpen() // 오진수 모델 띄움
     {
         modelChack = true;
@@ -56,54 +39,38 @@ public class RobbyButton : MonoBehaviour {
     }
     public void StopButton() // 오진수 GameObject UIbutton만 끄기
     {
-        roomMake.GetComponent<UIButton>().enabled = false;
         roomSearch.GetComponent<UIButton>().enabled = false;
         shop.GetComponent<UIButton>().enabled = false;
-        guild.GetComponent<UIButton>().enabled = false;
         character.GetComponent<UIButton>().enabled = false;
         coinPlus.GetComponent<UIButton>().enabled = false;
         jewelPlus.GetComponent<UIButton>().enabled = false;
-        mail.GetComponent<UIButton>().enabled = false;
-        set.GetComponent<UIButton>().enabled = false;
         modeling.SetActive(false);
     }
     public void StopGameObjectButton() // 오진수 RobbyButton GameObject 전부 끄기
     {
-
-        roomMake.SetActive(false);
         roomSearch.SetActive(false);
         shop.SetActive(false);
-        guild.SetActive(false);
         profile.SetActive(false);
         character.SetActive(false);
         modeling.SetActive(false);
     }
     public void StartView() // 오진수 처음 시작으로 초기화
     {
-        roomMake.GetComponent<UIButton>().enabled = true;
         roomSearch.GetComponent<UIButton>().enabled = true;
         shop.GetComponent<UIButton>().enabled = true;
-        guild.GetComponent<UIButton>().enabled = true;
         character.GetComponent<UIButton>().enabled = true;
         coinPlus.GetComponent<UIButton>().enabled = true;
         jewelPlus.GetComponent<UIButton>().enabled = true;
-        mail.GetComponent<UIButton>().enabled = true;
-        set.GetComponent<UIButton>().enabled = true;
 
-        roomMake.SetActive(true);
         roomSearch.SetActive(true);
         shop.SetActive(true);
-        guild.SetActive(true);
         profile.SetActive(true);
         character.SetActive(true);
 
-        room.SetActive(false);
         Maliborder.SetActive(false);
         setOption.SetActive(false);
         shopMenubor.SetActive(false);
         characterShopUIPanel.SetActive(false);
-        roomBorder.SetActive(false);
-        creatRoom.SetActive(false);
         if (modelChack == true) // 오진수 캐릭터가 구입되었을때 출력됨
         {
             modeling.SetActive(true);
