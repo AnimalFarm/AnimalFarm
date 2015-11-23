a0dad9f823662e2d392b3822e9a306253a3d500c
OHJinSu <qaqa12300@naver.com>
2015-11-23 21:16:01 +0900

    버튼 수정 완료

--- Assets/Script/RobbyButton.cs
+++ Assets/Script/RobbyButton.cs
@@ -8,31 +8,28 @@ public class RobbyButton : MonoBehaviour
     public UIButton ui_gameStart, ui_character, ui_shop; // 하단 버튼 게임시작,캐릭터,상점,랭킹
     public GameObject optionPopUp, finishPopUp, shopPopUp, characterPopUp, gameStartPopUp; // 각 버튼의 팝업
     public UIToggle shop_Character, shop_Gem, shop_Coin; // 상점을 열면 우선 보여주는 화면
-    public bool stateCheck = false;
-    public bool popUpCheck = false;
-    int count = 0;
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (shopPopUp.activeSelf == true)
+            if (shopPopUp.activeSelf)
             {
                 shopPopUp.SetActive(false);
             }
-            else if (optionPopUp.activeSelf == true)
+            else if (optionPopUp.activeSelf)
             {
                 optionPopUp.SetActive(false);
             }
-            else if (finishPopUp.activeSelf == true)
+            else if (finishPopUp.activeSelf)
             {
                 finishPopUp.SetActive(false);
             }
-            else if (characterPopUp.activeSelf == true)
+            else if (characterPopUp.activeSelf)
             {
                 characterPopUp.SetActive(false);
             }
-            else if (gameStartPopUp.activeSelf == true)
+            else if (gameStartPopUp.activeSelf)
             {
                 gameStartPopUp.SetActive(false);
             }
@@ -41,62 +38,54 @@ public class RobbyButton : MonoBehaviour
                 finishPopUp.SetActive(true);
             }
         }
-    }
-
-    public void OnOffButton(GameObject g)
-    {
-        count++;
-        stateCheck = !stateCheck;
-        popUpCheck = !popUpCheck;
-
-        if (stateCheck == true)
+        if (shopPopUp.activeSelf || characterPopUp.activeSelf || gameStartPopUp.activeSelf)
         {
             ui_gameStart.enabled = false;
+            ui_character.enabled = false;
+            ui_shop.enabled = false;
         }
         else
         {
             ui_gameStart.enabled = true;
+            ui_character.enabled = true;
+            ui_shop.enabled = true;
         }
-        
+    }
+
+    public void OnOffButton(GameObject g)
+    {        
         shopPopUp.SetActive(false);
         optionPopUp.SetActive(false);
         finishPopUp.SetActive(false);
         characterPopUp.SetActive(false);
         gameStartPopUp.SetActive(false);
 
-        if (count % 2 == 0) popUpCheck = true;
-
         switch (g.name)
         {
             case "Coinplus":
-                shopPopUp.SetActive(popUpCheck);
+                shopPopUp.SetActive(true);
                 shop_Coin.value = true;
                 break;
             case "Gemplus":
-                shopPopUp.SetActive(popUpCheck);
+                shopPopUp.SetActive(true);
                 shop_Gem.value = true;
                 break;
             case "Option":
-                optionPopUp.SetActive(popUpCheck);
+                optionPopUp.SetActive(true);
                 break;
             case "Close":
-                finishPopUp.SetActive(popUpCheck);
+                finishPopUp.SetActive(true);
                 break;
             case "Shop":
-                shopPopUp.SetActive(popUpCheck);
+                shopPopUp.SetActive(true);
                 shop_Character.value = true;
                 break;
             case "Character":
-                characterPopUp.SetActive(popUpCheck);
+                characterPopUp.SetActive(true);
                 break;
             case "Start":
-                gameStartPopUp.SetActive(popUpCheck);
+                gameStartPopUp.SetActive(true);
                 break;
-        }
-        if (count % 2 == 0)
-        {
-            popUpCheck = false;
-            count = 0;
         }
     }
 
@@ -108,6 +97,4 @@ public class RobbyButton : MonoBehaviour
     {
         Application.Quit();
     }
-
-
 }
