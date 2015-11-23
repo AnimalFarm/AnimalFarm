af66c25bf431e33b5fb0548d250994927a674707
OHJinSu <qaqa12300@naver.com>
2015-11-23 11:22:57 +0900

    UI수정

--- Assets/Script/RobbyButton.cs
+++ Assets/Script/RobbyButton.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 // 노승현, 로비 버튼 이벤트 스크립트
-public class RobbyButton : MonoBehaviour {
+public class RobbyButton : MonoBehaviour
+{
 
     public UIButton ui_gameStart, ui_character, ui_shop; // 하단 버튼 게임시작,캐릭터,상점,랭킹
     public GameObject optionPopUp, finishPopUp, shopPopUp, characterPopUp, gameStartPopUp; // 각 버튼의 팝업
+    public GameObject shop_Character, shop_Gem, shop_Coin; // 상점을 열면 우선 보여주는 화면
+    public GameObject shop_Panel, round_Panel; // 상점 , 게임시작 페널
     public bool stateCheck = false;
     public bool popUpCheck = false;
     int count = 0;
-
 
     public void OnOffButton(GameObject g)
     {
@@ -31,16 +33,22 @@ public class RobbyButton : MonoBehaviour {
         finishPopUp.SetActive(false);
         characterPopUp.SetActive(false);
         gameStartPopUp.SetActive(false);
+        shop_Panel.SetActive(false);
+        round_Panel.SetActive(false);
 
-        if(count%2 == 0) popUpCheck = true;
-      
+        if (count % 2 == 0) popUpCheck = true;
+
         switch (g.name)
         {
             case "Coinplus":
                 shopPopUp.SetActive(popUpCheck);
+                shop_Panel.SetActive(popUpCheck);
+                shop_Coin.GetComponentInChildren<UIToggle>().value = true;
                 break;
             case "Gemplus":
                 shopPopUp.SetActive(popUpCheck);
+                shop_Panel.SetActive(popUpCheck);
+                shop_Gem.GetComponentInChildren<UIToggle>().value = true;
                 break;
             case "Option":
                 optionPopUp.SetActive(popUpCheck);
@@ -50,9 +58,15 @@ public class RobbyButton : MonoBehaviour {
                 break;
             case "Shop":
                 shopPopUp.SetActive(popUpCheck);
+                shop_Panel.SetActive(popUpCheck);
+                shop_Character.GetComponentInChildren<UIToggle>().value = true;
                 break;
             case "Character":
                 characterPopUp.SetActive(popUpCheck);
+                break;
+            case "Start":
+                gameStartPopUp.SetActive(popUpCheck);
+                round_Panel.SetActive(popUpCheck);
                 break;
         }
         if (count % 2 == 0)
