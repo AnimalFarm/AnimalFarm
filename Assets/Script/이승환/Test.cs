4d297d4a16278ffb1ef37f49e851fd59ce649b6e
OHJinSu <qaqa12300@naver.com>
2015-11-26 05:07:43 +0900

    맵 모델링 추가

---
+++ Assets/Script/이승환/Test.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = Quaternion.Euler(0f, 0f, -90f);
+	}
+}
