587e047c94294345e9d265daeed2575254311794
OHJinSu <qaqa12300@naver.com>
2015-12-04 00:39:21 +0900

    UI에 3D 모델링 출력 노승현

---
+++ Assets/Script/CameraSize.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSize : MonoBehaviour {
+
+	
+    void Awake()
+    {
+        gameObject.GetComponent<Camera>().orthographicSize = (Screen.height * 1280f / 720f) / Screen.width; 
+	}
+}
