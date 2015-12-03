819f16e0df90a7aebc1c9e561a77889b1a3879e2
lsh2217 <97575756@naver.com>
2015-12-04 02:45:12 +0900

    체력감소 싱크수정

--- Assets/Script/Player/AttackChk.cs
+++ Assets/Script/Player/AttackChk.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class AttackChk : MonoBehaviour {
 
-    public GameObject Enemy;    //오브잭트
-    public GameObject Player;   //오브잭트
+    public GameObject Enemy;    //이승환//보스 오브잭트
+    public GameObject Player;   //이승환//플레이어 오브잭트
+ 
     float timer=0;
 
     void Awake()
@@ -14,12 +15,13 @@ public class AttackChk : MonoBehaviour {
     void Update()
     {
         timer += Time.deltaTime;
+       // Player.GetComponent<BoxCollider>().enabled = false;
     }
      void OnTriggerEnter(Collider order)//이승환//오브젝트와 콜리더가 처음 충돌했을 때 한번 한번호출
      {
-         if (order.tag == "Enemy" && order.tag != "Player" && Player.GetComponent<PlayerAttack>().bAttack == true)
+         if (order.tag == "Enemy" && order.tag != "Player")
         {
-            if (timer > 0.5f)
+            if (timer > 0.45f)
             {
                 Enemy.GetComponent<Eenemymove>().test.fillAmount -= 10f * 0.01f;
                 Enemy.GetComponent<Eenemymove>().Hp -= 10f;
--- Assets/Script/Player/PlayerAttack.cs
+++ Assets/Script/Player/PlayerAttack.cs
@@ -5,24 +5,31 @@ public class PlayerAttack : MonoBehaviour {
     public Animator _Playre;
     public bool bAttack = false;
     public float damig =10;
-
+    public GameObject Sword;
     float timer;
+    void Awake()
+    {
+        //Sword.GetComponent<BoxCollider>().enabled = false;
+    }
 	void Update () {
         if (bAttack)
         {
             timer += Time.deltaTime;
-            if (timer > 0.1f)
+            if (timer > 0.25f)
             {
+                Sword.GetComponent<BoxCollider>().enabled = true;
                 _Playre.ResetTrigger("bAttack_01");
                 timer = 0f;
                 bAttack = false;
             }
+           
         }
+       
 	}
 
     public void Attack_()
     {
-
+        Sword.GetComponent<BoxCollider>().enabled = false; 
         _Playre.SetTrigger("bAttack_01");
         bAttack = true;
     }
