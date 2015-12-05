8693493b98653d1b7d91c1d2376537fd02bf37c3
NSH <gus9583@naver,cin>
2015-12-05 23:39:32 +0900

    케릭터 체력바 이미지

--- Assets/Script/PlayerCharacter.cs
+++ Assets/Script/PlayerCharacter.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class PlayerCharacter : MonoBehaviour {
 
     public GameObject user_Bear, user_rabbit, user_panda, user_dog, user_penguin;
-
+    public UISprite PlayerHpBar;
     
     void Awake()
     {
@@ -19,18 +19,23 @@ public class PlayerCharacter : MonoBehaviour {
         {
             case 1:
                 user_Bear.SetActive(true);
+                PlayerHpBar.spriteName = "BearCondition";
                 break;
             case 2:
                 user_dog.SetActive(true);
+                PlayerHpBar.spriteName = "DogCondition";
                 break;
             case 3:
                 user_panda.SetActive(true);
+                PlayerHpBar.spriteName = "PandaCondition";
                 break;
             case 4:
                 user_penguin.SetActive(true);
+                PlayerHpBar.spriteName = "PenguinCondition";
                 break;
             case 5:
                 user_rabbit.SetActive(true);
+                PlayerHpBar.spriteName = "RabbitCondition";
                 break;
         }
     }
