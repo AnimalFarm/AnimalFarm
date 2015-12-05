a75c0d28429363b74ed4d268f55be1e7598e0805
OHJinSu <qaqa12300@naver.com>
2015-12-06 07:09:14 +0900

    캐릭터 대사 추가 노승현

--- Assets/NGUI/Examples/Scripts/Other/SpinWithMouse.cs
+++ Assets/NGUI/Examples/Scripts/Other/SpinWithMouse.cs
@@ -5,9 +5,24 @@ public class SpinWithMouse : MonoBehaviour
 {
 	public Transform target;
 	public float speed = 1f;
-
+    int number = 0;
+    public Lines lines;
+    
 	void OnDrag (Vector2 delta)
 	{
         target.localRotation = Quaternion.Euler(0f, -0.5f * delta.x * speed, 0f) * target.localRotation;   
 	}
+    void OnClick()
+    {
+        ++number;
+        if (number == 1) lines.LinesText(number);
+        if (number == 2) lines.LinesText(number);
+        if (number == 3) lines.LinesText(number);
+        if (number == 4) lines.LinesText(number);
+        if (number == 5)
+        {
+            lines.LinesText(number);
+            number = 0;
+        }    
+    }
 }
